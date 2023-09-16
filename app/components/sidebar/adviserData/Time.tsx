9752aaa82fd9852a8ca8a0100a222a7f24e6a480
Tyrone <tvalverdes9@gmail.com>
2023-09-16 18:55:38 -0500

    icon fixed

--- app/components/sidebar/adviserData/Time.tsx
+++ app/components/sidebar/adviserData/Time.tsx
@@ -1,9 +1,9 @@
-import AccessTimeFilledOutlinedIcon from '@mui/icons-material/AccessTimeFilledOutlined'
+import AccessTimeFilledOutlined from '@mui/icons-material/AccessTimeFilledOutlined'
 
 export const Time = () => {
   return (
     <span className="text-black border-2 border-black w-fit px-2 text-xs -tracking-tighter rounded-md">
-      <AccessTimeFilledOutlinedIcon className="text-sm" /> 50 m
+      <AccessTimeFilledOutlined className="text-sm" /> 50 m
     </span>
   )
 }
