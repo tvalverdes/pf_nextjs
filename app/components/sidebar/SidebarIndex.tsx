6fd415cb5eaa65c6e068e373459fef056a5fe1f1
Tyrone <tvalverdes9@gmail.com>
2023-09-19 20:31:01 -0500

    minor fix

--- app/components/sidebar/SidebarIndex.tsx
+++ app/components/sidebar/SidebarIndex.tsx
@@ -1,14 +1,29 @@
 'use client'
 import Calendar from './Calendar'
-import { InputField } from './InputField'
 import { PayButton } from './PayButton'
 import { AdviserData } from './adviserData'
+import { TextField } from '@mui/material'
+
 export const SidebarIndex = () => {
   return (
     <div className="w-full flex flex-col gap-4 bg-white">
       <AdviserData />
-      <InputField name="Nombre" placeholder="Ingresa tu nombre" />
-      <InputField name="Correo" placeholder="example@gmail.com" />
+      <TextField
+        required
+        fullWidth
+        id="standard-required"
+        label="Nombre"
+        placeholder="Ingresa tu nombre"
+        variant="outlined"
+      />
+      <TextField
+        required
+        fullWidth
+        id="standard-required"
+        label="Correo"
+        placeholder="Ingresa tu correo"
+        variant="outlined"
+      />
       <Calendar />
       <PayButton />
     </div>
