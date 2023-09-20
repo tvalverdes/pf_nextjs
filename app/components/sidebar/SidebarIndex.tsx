59922fe5d7cbd417cce5f7439c39d7d8db076da7
Tyrone <tvalverdes9@gmail.com>
2023-09-19 22:36:45 -0500

    hook-form added

--- app/components/sidebar/SidebarIndex.tsx
+++ app/components/sidebar/SidebarIndex.tsx
@@ -3,29 +3,35 @@ import Calendar from './Calendar'
 import { PayButton } from './PayButton'
 import { AdviserData } from './adviserData'
 import { TextField } from '@mui/material'
+import { useForm } from 'react-hook-form'
 
 export const SidebarIndex = () => {
+  const { register } = useForm()
   return (
-    <div className="w-full flex flex-col gap-4 bg-white">
+    <>
       <AdviserData />
-      <TextField
-        required
-        fullWidth
-        id="standard-required"
-        label="Nombre"
-        placeholder="Ingresa tu nombre"
-        variant="outlined"
-      />
-      <TextField
-        required
-        fullWidth
-        id="standard-required"
-        label="Correo"
-        placeholder="Ingresa tu correo"
-        variant="outlined"
-      />
-      <Calendar />
-      <PayButton />
-    </div>
+      <form className="w-full flex flex-col gap-4 bg-white">
+        <TextField
+          required
+          fullWidth
+          id="standard-required"
+          label="Nombre"
+          placeholder="Ingresa tu nombre"
+          variant="outlined"
+          {...register('name')}
+        />
+        <TextField
+          required
+          fullWidth
+          id="standard-required"
+          label="Correo"
+          placeholder="Ingresa tu correo"
+          variant="outlined"
+          {...register('mail')}
+        />
+        <Calendar />
+        <PayButton />
+      </form>
+    </>
   )
 }
