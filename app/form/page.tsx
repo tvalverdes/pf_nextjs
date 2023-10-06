c9077632d93d1a460fe56789495027d2fb264260
Tyrone <tvalverdes9@gmail.com>
2023-10-06 10:00:18 -0500

    form route added

---
+++ app/form/page.tsx
@@ -0,0 +1,10 @@
+import { SidebarIndex } from '../components/sidebar/SidebarIndex'
+
+export default function Form() {
+  return (
+    <div className="flex flex-col w-full md:w-2/4 px-3 pt-2 bg-white h-full lg:w-2/4 xl:md:w-1/4 md:px-5">
+      <p>HORARIOS DE ANDRÉS</p>
+      <SidebarIndex />
+    </div>
+  )
+}
--- app/success/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Success() {
-  return (
-    <>
-      <main></main>
-    </>
-  )
-}
